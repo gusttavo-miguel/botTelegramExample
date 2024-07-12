990b2e7deef337091415e48f7cb2bfd1ac80f7b1
Gustavo Miguel <77859657+gusttavo-miguel@users.noreply.github.com>
2024-07-12 19:55:15 -0300

    Commit inicial

---
+++ botTelegramExample/src/main/java/com/example/botTelegram/RobotData.java
@@ -0,0 +1,6 @@
+package com.example.botTelegram;
+
+public class RobotData {
+    public static final String BOT_USER_NAME = "Incident_Monitoring_Bot";
+    public static final String BOT_TOKEN = "7276153917:AAG4dSeUXoksGeRV7CQOsVs37VLzyQF915Q";
+}
